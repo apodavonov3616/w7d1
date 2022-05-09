8341827a331adeb1ff6b4e2ae081b9bafa268280
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-09 17:20:29 -0400

    midway into application record

--- 99_cats/app/controllers/sessions_controller.rb
+++ 99_cats/app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
     end
     
     def create
-        
+        @user = User.find_by_credentials(params([user][username]), params([user][password])
+        @user.reset_session_token!
+        redirect_to cats_url
     end
+
+    def destroy
+        if @current_user
+            @current_user.reset_session_token!
+        end
+        session[:session_token] = nil
+    end     
     
 end
--- 99_cats/app/controllers/users_controller.rb
+++ 99_cats/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            redirect_to user_url(user)
+            render json: user
         else
             render json: @user.errors.full_message, status: unprocessable_entity
         end
